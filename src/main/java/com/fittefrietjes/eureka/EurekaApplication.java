b56fd4836aa19ffc8b837806528bf269cb0b90a7
Niels Korporaal <napkorporaal@icloud.com>
2021-05-05 21:52:02 +0200

    :pencil2: Renamed project and package gateway to eureka-service

--- src/main/java/com/fittefrietjes/eureka/EurekaApplication.java
+++ src/main/java/com/fittefrietjes/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.fittefrietjes.gateway;
+package com.fittefrietjes.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/fittefrietjes/eureka/EurekaApplicationTests.java
+++ src/test/java/com/fittefrietjes/eureka/EurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fittefrietjes.gateway;
+package com.fittefrietjes.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
